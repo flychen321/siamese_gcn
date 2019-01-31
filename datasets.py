e55fc91e3865d003715fc7516abb9b79c58741db
dl <571435345@qq.com>
2019-01-31 22:17:44 +0800

    refine datasets dataloader

--- datasets.py
+++ datasets.py
@@ -4,30 +4,8 @@ from torchvision import datasets, models, transforms
 from torch.utils.data import Dataset
 from torch.utils.data.sampler import BatchSampler
 import torch
+from torchvision.datasets.folder import default_loader
 
-
-def pil_loader(path):
-    # open path as file to avoid ResourceWarning (https://github.com/python-pillow/Pillow/issues/835)
-    with open(path, 'rb') as f:
-        img = Image.open(f)
-        return img.convert('RGB')
-
-
-def accimage_loader(path):
-    import accimage
-    try:
-        return accimage.Image(path)
-    except IOError:
-        # Potentially a decoding problem, fall back to PIL.Image
-        return pil_loader(path)
-
-
-def default_loader(path):
-    from torchvision import get_image_backend
-    if get_image_backend() == 'accimage':
-        return accimage_loader(path)
-    else:
-        return pil_loader(path)
 
 
 class SiameseDataset(Dataset):
